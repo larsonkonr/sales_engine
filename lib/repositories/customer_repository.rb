94702bef12ac40d12a734c040e35667dc24b2cd8
Kalah Cheek <kcheek7@ymail.com>
2014-10-07 10:35:19 -0600

    began making repository

--- lib/repositories/customer_repository.rb
+++ lib/repositories/customer_repository.rb
@@ -2,6 +2,28 @@ require_relative '../customer'
 
 class CustomerRepository
 
-  
+  def initialize(customers = [])
+    @customers = customers
+  end
+
+  def find_by_first_name(first_name)
+    customers.select { |customer| customer.first_name == first_name}
+  end
+
+  def find_by_last_name(last_name)
+    customers.select { |customer| customer.last_name == last_name }
+  end
+
+  def find_by_id(id)
+    customers.select { |customer| customer.id == id }
+  end
+
+  def find_by_created_at(created_at)
+    customers.select {|customer| customer.created_at == created_at}
+  end
+
+  def find_by_updated_at(updated_at)
+    customers.select {|customer| customer.updated_at == updated_at}
+  end
 
 end
