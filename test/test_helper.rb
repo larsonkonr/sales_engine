00dc16463094664ee7d0d7482634192fb1f630a1
Konr Larson <larsonkonr@gmail.com>
2014-10-07 08:45:18 -0600

    added files and some structure

---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+gem 'minitest', '~> 5.2'
+require 'simplecov'; SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
---
+++ test/transaction_repository_test.rb
@@ -0,0 +1,30 @@
+require_relative 'test_helper'
+
+class TransactionRepositoryTest < Minitest::Test
+
+  def setup
+    @transaction = TransactionRepository.new
+  def test_transaction_has_id
+
+    assert 
+  end
+  def test_transaction_has_invoice_id
+
+  end
+  def test_transaction_has_credit_card_number
+
+  end
+  def test_transaction_has_credit_card_expiration_date
+
+  end
+  def test_transaction_has_a_result
+
+  end
+  def test_transaction_has_created_at
+
+  end
+  def test_transaction_has_updated_at
+
+  end
+
+end
