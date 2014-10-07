f60276ec98864f9de12502ddbc6c225eb38f7d92
Kalah Cheek <kcheek7@ymail.com>
2014-10-07 09:57:20 -0600

    changed the filepath so it works

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,8 +1,12 @@
 require "csv"
+require './lib/customer'
 
 class Parser
-    def self.load_file(filepath = "data/")
-      contents = CSV.open(filepath, headers: true, header_converters: :symbol)
-    end
+  def self.load_file(filepath = "../data/")
+    contents = CSV.open(filepath, headers: true, header_converters: :symbol)
+  end
 
+  def self.load_customers(filepath)
+    [Customer.new]
+  end
 end
