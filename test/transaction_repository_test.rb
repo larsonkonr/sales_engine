401ea9aec04de83b9deca769ef99fb1e62cb25b6
Kalah Cheek <kcheek7@ymail.com>
2014-10-08 14:17:49 -0600

    edited the require relatives

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -1,4 +1,7 @@
-require_relative 'test_helper'
+gem 'minitest', '~> 5.2'
+require 'simplecov'; SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
 require 'stringio'
 require_relative '../lib/repositories/transaction_repository'
 
